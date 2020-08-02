468ac98eca5f5fa10b4db64d532ed0c13b99e9c6
Akash Barman <akash.barman@engineer.ai>
2020-08-02 17:23:58 +0530

    unsubscribe subscription

--- src/app/components/equipment-catalog/equipment-catalog.component.ts
+++ src/app/components/equipment-catalog/equipment-catalog.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CatalogService } from 'src/app/services/catalog.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-equipment-catalog',
   templateUrl: './equipment-catalog.component.html',
   styleUrls: ['./equipment-catalog.component.css']
 })
-export class EquipmentCatalogComponent implements OnInit {
+export class EquipmentCatalogComponent implements OnInit, OnDestroy {
   public equipmentList: any;
+  subs: Subscription;
   constructor(
     private route: ActivatedRoute,
     public catalogService: CatalogService
     ) {
-    this.route.paramMap.subscribe( paramMap => {
+    this.subs = this.route.paramMap.subscribe( paramMap => {
       this.catalogService.locationId = paramMap.get('locationId');
       this.catalogService.branchId = paramMap.get('branchId');
       this.catalogService.category = paramMap.get('category');
@@ -39,6 +41,10 @@ export class EquipmentCatalogComponent implements OnInit {
         image: `subcategory/${arrObj.image}`
       };
     });
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
 }
